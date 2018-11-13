a8e3b6ddb5031062b363956bfd6303ac4f2fa4e4
Sanford Miller <skm2159@columbia.edu>
2018-11-13 03:08:05 -0500

    pretty printing works until new errors in semant.ml

---
+++ src/ast.ml
@@ -0,0 +1,100 @@
+type operator = Add | Sub | Mul | Div | Exp | Eq | Neq | Less | Leq | Greater | Geq | And | Or
+
+and uop = Neg | Not
+
+and literal =
+  | IntLit of int
+  | BoolLit of bool
+  | FloatLit of float
+  | StringLit of string
+
+and typ = Int | Float | Bool | String | Dyn | IntArr | FloatArr | BoolArr | StringArr | FuncType | Null
+
+and bind = Bind of string * typ
+
+and expr =
+  | Binop of expr * operator * expr
+  | Lit of literal
+  | Var of bind
+  | Unop of uop * expr
+  | Call of string * expr list
+  | Method of expr * string * expr list
+  | Field of expr * string
+  | List of expr list
+
+and stmt =
+  | Func of bind * bind list * stmt
+  | Block of stmt list
+  | Expr of expr
+  | If of expr * stmt * stmt
+  | For of bind * expr * stmt
+  | While of expr * stmt
+  | Return of expr
+  | Class of string * stmt
+  | Asn of bind list * expr
+  | TypeInfo of expr
+  | Nop
+
+let rec string_of_op = function
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
+  | Exp -> "**"
+  | Eq -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "and"
+  | Or -> "or"
+
+let rec string_of_uop = function
+  | Neg -> "-"
+  | Not -> "not"
+
+let rec string_of_lit = function
+  | IntLit(i) -> string_of_int i
+  | BoolLit(b) -> string_of_bool b
+  | FloatLit(f) -> string_of_float f
+  | StringLit(s) -> s
+
+let rec string_of_typ = function
+  | Int -> "int"
+  | Float -> "float"
+  | Bool -> "bool"
+  | String -> "str"
+  | Dyn -> "dyn"
+  | IntArr -> "int[]"
+  | FloatArr -> "float[]"
+  | BoolArr -> "bool[]"
+  | StringArr -> "str[]"
+  | FuncType -> "funct"
+  | Null -> "null"
+
+let rec string_of_bind = function
+    | Bind(s, t) -> s ^ ": " ^ string_of_typ t
+
+let rec string_of_expr = function
+  | Binop(e1, o, e2) -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Lit(l) -> string_of_lit l
+  | Var(b) -> string_of_bind b
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Call(f, el) -> f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Method(obj, m, el) -> string_of_expr obj ^ "." ^ m ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Field(obj, s) -> string_of_expr obj ^ "." ^ s
+  | List(el) -> String.concat ", " (List.map string_of_expr el)
+
+let rec string_of_stmt = function
+  | Func(b, bl, s) -> "def " ^ string_of_bind b ^ "(" ^ String.concat ", " (List.map string_of_bind bl) ^ ")\n" ^ string_of_stmt s
+  | Block(sl) -> String.concat "\n" (List.map string_of_stmt sl) ^ "\n"
+  | Expr(e) -> string_of_expr e
+  | If(e, s1, s2) ->  "if " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s1 ^ "else:\n" ^ string_of_stmt s2
+  | For(b, e, s) -> "for " ^ string_of_bind b ^ " in " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s
+  | While(e, s) -> "while " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s
+  | Return(e) -> "return " ^ string_of_expr e ^ "\n"
+  | Class(str, s) -> "class " ^ str ^ ":\n" ^ string_of_stmt s
+  | Asn(bl, e) -> String.concat ", " (List.map string_of_bind bl) ^ " = "  ^ string_of_expr e
+  | TypeInfo(e) -> string_of_expr e
+  | Nop -> ""
---
+++ src/sast.ml
@@ -0,0 +1,72 @@
+open Ast
+
+(*
+
+What semant does:
+
+  1. undeclared identifiers (get a list of globals and locals for each function, check to make sure they are all declared at some poiont
+  2. correct number of arguments in function call
+  3. correct types of explicitly declared variables
+  4. return types in the right place
+  5. duplicate formal arguments
+*)
+
+type sbind =
+  | WeakBind of string * typ (* Not known to be declared, typ can be dynamic *)
+  | StrongBind of string * typ (* Known to be declared, typ can be dynamic *)
+
+and sexpr =
+  | SBinop of sexpr * operator * sexpr
+  | SLit of literal
+  | SVar of sbind
+  | SUnop of uop * sexpr
+  | SCall of sbind * sexpr list * sstmt (* sstmt is type-checked version of function (SFunc for the types passed to that call) *)
+  | SMethod of sexpr * string * sexpr list
+  | SField of sexpr * string
+  | SList of sexpr list * typ
+  | SNoexpr 
+
+and sstmt = (* this can be refactored using Blocks, but I haven't quite figured it out yet *)
+  | SFunc of sbind * sbind list * sbind list * sstmt (* string list is list of locals *)
+  | SFuncDecl of sbind * sbind list * sstmt
+  | SBlock of sstmt list 
+  | SExpr of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sbind * sexpr * sstmt
+  | SWhile of sexpr * sstmt
+  | SReturn of sexpr
+  | SClass of sbind * sstmt
+  | SAsn of sbind list * sexpr
+  | SNop
+
+and sprogram = sstmt list * sbind list
+
+let rec string_of_sbind = function
+  | WeakBind(s, t) -> s ^ ": (weak) " ^ string_of_typ t
+  | StrongBind(s, t) -> s ^ ": (strong) " ^ string_of_typ t
+
+and string_of_sexpr = function
+  | SBinop(e1, o, e2) -> string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+  | SLit(l) -> string_of_lit l
+  | SVar(b) -> string_of_sbind b
+  | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
+  | SCall(f, el, s) -> string_of_sbind f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ "):\n" ^ string_of_sstmt s
+  | SMethod(obj, m, el) -> string_of_sexpr obj ^ "." ^ m ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SField(obj, s) -> string_of_sexpr obj ^ "." ^ s
+  | SList(el, t) -> string_of_typ t ^ " list : " ^ String.concat ", " (List.map string_of_sexpr el)
+  | SNoexpr -> ""
+
+and string_of_sstmt = function
+  | SFunc(b, bl1, bl2, s) -> "def " ^ string_of_sbind b ^ "(" ^ String.concat ", " (List.map string_of_sbind bl1) ^ ")\n" ^ string_of_sstmt s
+  | SBlock(sl) -> String.concat "\n" (List.map string_of_sstmt sl) ^ "\n"
+  | SExpr(e) -> string_of_sexpr e
+  | SIf(e, s1, s2) ->  "if " ^ string_of_sexpr e ^ ":\n" ^ string_of_sstmt s1 ^ "else:\n" ^ string_of_sstmt s2
+  | SFor(b, e, s) -> "for" ^ string_of_sbind b ^ " in " ^ string_of_sexpr e ^ ":\n" ^ string_of_sstmt s
+  | SWhile(e, s) -> "while " ^ string_of_sexpr e ^ ":\n" ^ string_of_sstmt s
+  | SReturn(e) -> "return " ^ string_of_sexpr e ^ "\n"
+  | SClass(b, s) -> "class " ^ ((function | StrongBind(s, t) -> s | WeakBind(s, t) -> s) b ^ ":\n" ^ string_of_sstmt s)
+  | SAsn(bl, e) -> String.concat ", " (List.map string_of_sbind bl) ^ " = "  ^ string_of_sexpr e
+  | SFuncDecl(b, bl, s) -> "def " ^ string_of_sbind b ^ "(" ^ String.concat ", " (List.map string_of_sbind bl) ^ ")\n" ^ string_of_sstmt s
+  | SNop -> ""
+
+and string_of_sprogram (sl, bl) = String.concat "" (List.map string_of_sstmt sl) ^ "\n\n\n" ^ String.concat "" (List.map string_of_sbind bl)
