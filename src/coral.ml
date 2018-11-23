739d2148d4d1f5bdeb51ea0ed76c36f0033f4102
Sanford Miller <skm2159@columbia.edu>
2018-11-23 14:35:58 -0600

    still segfaults smh

--- src/coral.ml
+++ src/coral.ml
@@ -269,8 +269,9 @@ let rec file map smap fname run = (* todo combine with loop *)
     | Failure explanation -> Printf.printf "%s\n" explanation; flush stdout
 ;;
 
-(* main loop of the interpreter *)
-let _ = 
+(* main loop *)
+let _ =
+  print_endline "XXXXXXXXX";
   parse_cmdline specs print_endline; (* parse command line arguments *)
   let emptymap = StringMap.empty in let semptymap = StringMap.empty in
   if String.length !fpath = 0 then 
