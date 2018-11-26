f15d4588b4e5db5082d684920e4c1bdef0df70bb
Jacob Austin <ja3067@columbia.edu>
2018-11-26 16:37:38 -0500

    fixed bug in function assignment

--- src/semant.ml
+++ src/semant.ml
@@ -260,27 +260,28 @@ and func_stmt globals locals stack flag = function
       | [] -> ()
       | (Bind(n1, _) :: Bind(n2, _) :: _) when n1 = n2 -> raise (Failure ("SyntaxError: duplicate argument '" ^ n1 ^ "' in function definition"))
       | _ :: t -> dups t
-    in let _ = dups (List.sort (fun (Bind(a, _)) (Bind(b, _)) -> compare a b) b) in let Bind(x, t) = a in 
-    let map' = StringMap.add x (Dyn, FuncType, true, Some(Func(a, b, c))) locals in 
-
-    let semantmap = StringMap.add x (Dyn, FuncType, true, Some(Func(a, b, c))) StringMap.empty in (* empty map for semantic checking *)
+    in let _ = dups (List.sort (fun (Bind(a, _)) (Bind(b, _)) -> compare a b) b) in let Bind(name, btype) = a in 
+    
+    let (map', _) = check_assign locals (FuncType, SNoexpr, Some(Func(a, b, c))) (Bind(name, Dyn)) in
+    let (semantmap, _) = check_assign StringMap.empty (FuncType, SNoexpr, Some(Func(a, b, c))) (Bind(name, Dyn)) in (* empty map for semantic checking *)
 
     let (map'', bind) = List.fold_left (fun (map, out) (Bind(x, t)) -> let (map', bind) = check_assign map (t, SNoexpr, None) (Bind(x, t)) in (map', bind :: out)) (semantmap, []) b in
     let bindout = List.rev bind in
     let (map2, block, data, locals) = (func_stmt map'' map'' TypeMap.empty true c) in
       (match data with
-        | Some (typ2, e', d) -> let Bind(name, btype) = a in 
+        | Some (typ2, e', d) ->
           if btype <> Dyn && btype <> typ2 then if typ2 <> Dyn then 
           raise (Failure ("STypeError: invalid return type")) else 
           let func = { styp = btype; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
-            (map', SFunc(func), None, [StrongBind(x, FuncType)]) 
+            (map', SFunc(func), None, [StrongBind(name, FuncType)]) 
           else let func = { styp = typ2; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
-          (map', SFunc(func), None, [StrongBind(x, FuncType)])
+          (map', SFunc(func), None, [StrongBind(name, FuncType)])
       
-        | None -> let Bind(name, btype) = a in if btype <> Dyn then 
+        | None -> 
+          if btype <> Dyn then 
           raise (Failure ("STypeError: invalid return type")) else 
           let func = { styp = Null; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
-          (map', SFunc(func), None, [StrongBind(x, FuncType)]))
+          (map', SFunc(func), None, [StrongBind(name, FuncType)]))
 
   | If(a, b, c) -> let (typ, e', _) = func_expr globals locals stack flag a in 
         let (map', value, data, out) = func_stmt globals locals stack flag b in 
@@ -320,16 +321,16 @@ and stmt map = function (* evaluates statements, can pass it a func *)
       | [] -> ()
       | (Bind(n1, _) :: Bind(n2, _) :: _) when n1 = n2 -> raise (Failure ("SyntaxError: duplicate argument '" ^ n1 ^ "' in function definition"))
       | _ :: t -> dups t
-    in let _ = dups (List.sort (fun (Bind(a, _)) (Bind(b, _)) -> compare a b) b) in let Bind(x, t) = a in 
-    let map' = StringMap.add x (Dyn, FuncType, true, Some(Func(a, b, c))) map in 
-
-    let semantmap = StringMap.add x (Dyn, FuncType, true, Some(Func(a, b, c))) StringMap.empty in (* empty map for semantic checking *)
+    in let _ = dups (List.sort (fun (Bind(a, _)) (Bind(b, _)) -> compare a b) b) in let Bind(name, btype) = a in 
+    
+    let (map', _) = check_assign map (FuncType, SNoexpr, Some(Func(a, b, c))) (Bind(name, Dyn)) in
+    let (semantmap, _) = check_assign StringMap.empty (FuncType, SNoexpr, Some(Func(a, b, c))) (Bind(name, Dyn)) in (* empty map for semantic checking *)
 
     let (map'', bind) = List.fold_left (fun (map, out) (Bind(x, t)) -> let (map', bind) = check_assign map (t, SNoexpr, None) (Bind(x, t)) in (map', bind :: out)) (semantmap, []) b in
     let bindout = List.rev bind in
     let (map2, block, data, locals) = (func_stmt map'' map'' TypeMap.empty true c) in
       (match data with
-        | Some (typ2, e', d) -> let Bind(name, btype) = a in 
+        | Some (typ2, e', d) ->
             if btype <> Dyn && btype <> typ2 then if typ2 <> Dyn then 
             raise (Failure ("STypeError: invalid return type")) else 
             let func = { styp = btype; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
@@ -337,7 +338,8 @@ and stmt map = function (* evaluates statements, can pass it a func *)
               let func = { styp = typ2; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
             (map', SFunc(func), [StrongBind(name, FuncType)])
         
-        | None -> let Bind(name, btype) = a in if btype <> Dyn then 
+        | None -> 
+          if btype <> Dyn then 
           raise (Failure ("STypeError: invalid return type")) else 
           let func = { styp = Null; sfname = name; sformals = (List.rev bindout); slocals = locals; sbody = block } in 
           (map', SFunc(func), [StrongBind(name, FuncType)]))
